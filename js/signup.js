2a61056d8d8c458265ffae3816d268291bace6e8
Brandon Chansamone <braakson@yahoo.com>
2021-03-17 22:48:15 -0600

    edied

--- js/signup.js
+++ js/signup.js
@@ -1,50 +1,32 @@
-function func() {  
-
-    $(document).ready(function () {
-
-        $('#question').on('change', function () {
-
-            if (this.value == 'What is your mothers maiden name?') {
-
-                $("#text").show();
-
-            }
-
-            else if (this.value == 'What is you oldest brother middle name??') {
-
-                $("#text").show();
-
-            }
-
-            else if (this.value == "What is your hometown?") {
-
-                $("#text").show();
-
-            }
-
-            else {
-
-                $("#text").hide();
-
-            }
-
-
-        });
-
-    });
-
-}
-function showQuestionText(){
-    var selectQuestion=document.querySelector("#selectQuestion");
-    var txtQuestion1=document.querySelector("#txtQuestion1");
+function showQuestionText() {
+    var selectQuestion = document.querySelector("#selectQuestion");
+    var txtQuestion1 = document.querySelector("#txtQuestion1");
     if(selectQuestion.value !="----Please choose an option----"){
         txtQuestion1.classList.remove("invisible");
     }
-    else{
-        txtQuestion1.classList.add("invisible");
-    }
+    // if(selectQuestion.value =="----Please choose an option----"){
+    //     txtQuestion1.classList.add("invisible");
+    // }
+
+    // if (selectQuestion == "What is your mother's maiden name?") {
+    //     txtQuestion1.classList.remove("invisible");
+    // }
+    // if (selectQuestion == "What is your oldest brother middle name?") {
+    //     txtQuestion1.classList.remove("invisible");
+    // }
+    // if (selectQuestion == "What is your hometown?") {
+    //     txtQuestion1.classList.remove("invisible");
+    // }
+    // else {
+    //     txtQuestion1.classList.add("invisible");
+    // }
+
+    // else{
+    //     txtQuestion1.classList.add("invisible");
+    // }
+
 }
 
-function validateForm(){
+function validateForm() {
 
 }
