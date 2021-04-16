3f9b433155e996be953deb073fe6f49fe7bd5bd3
Brandon Chansamone <braakson@yahoo.com>
2021-04-15 22:01:18 -0600

    posts working

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -23,22 +23,70 @@ var friends=[
         hobby: "Puzzles"
     }
 ]
+const Post = require("../models/post");
 
-exports.showFriends =(req,res) => {
-    res.render("friends",{listedFriends:friends});
-}
+// exports.showFriends =(req,res) => {
+//     res.render("friends",{listedFriends:friends});
+// }
 
-exports.getSignin =(req,res) => {
-    res.render("signin");
-}
+// exports.getSignin =(req,res) => {
+//     res.render("signin");
+// }
 
-exports.showIndex = (req, res) => {
-    res.render("index");
-}
-exports.showHomepage = (req, res) => {
-    res.render("homepage");
-}
+// exports.showIndex = (req, res) => {
+//     res.render("index");
+// }
+// exports.showHomepage = (req, res) => {
+//     res.render("homepage");
+// }
 
-exports.showSignUp = (req, res) => {
-    res.render("signup")
+// exports.showSignUp = (req, res) => {
+//     res.render("signup");
+// }
+
+
+// exports.getPosts = (req,res) =>{
+//     Post.find()
+//             .then(posts => {
+//                 res.locals.posts = posts;
+//                 homePosts=posts;
+//                 console.log(homePosts)
+//                 next()
+//             })
+//             .catch(error => {
+//                 console.log(`Error gathering post data: ${error.message}`)
+//                 next(error);
+//             })
+// }
+
+
+module.exports = {
+    index: (req, res, next) => {
+        Post.find()
+            .then(posts => {
+                res.locals.posts = posts;
+                homePosts=posts;
+                console.log(homePosts)
+                next()
+            })
+            .catch(error => {
+                console.log(`Error gathering post data: ${error.message}`)
+                next(error);
+            })
+    },
+    showFriends: (req,res) =>{
+        res.render("friends",{listedFriends:friends});
+    },
+    getSignin: (req,res) =>{
+        res.render("signin");
+    },
+    showIndex: (req,res) =>{
+        res.render("index");
+    },
+    showHomepage: (req,res) =>{
+        res.render("homepage");
+    },
+    showSignUp: (req,res) =>{
+        res.render("signup")
+    }
 }
