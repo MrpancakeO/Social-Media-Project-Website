e9986a08f87962c4043a76b1676a8b9cf8527398
Brandon Chansamone <braakson@yahoo.com>
2021-05-13 20:21:29 -0600

    reverted main

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 const express= require("express"),
 app=express(),
-router = require("./routes/index"),
+router = express.Router(),
 homeController=require("./controllers/homeController"),
 errorController = require("./controllers/errorController"),
 usersController = require("./controllers/usersController"),
@@ -28,21 +28,21 @@ mongoose.connect("mongodb://localhost:27017/Social_Media_BAS",
 app.set("port", process.env.PORT || 3000);
 app.set("view engine", "ejs");
 
-app.use(express.static("public"));
-app.use(layouts);
-app.use(expressValidator());
-app.use(
+router.use(express.static("public"));
+router.use(layouts);
+router.use(expressValidator());
+router.use(
     express.urlencoded({
         extended: false
     })
 );
 
-app.use(methodOverride("_method", {methods: ['POST', 'GET']}));
+router.use(methodOverride("_method", {methods: ['POST', 'GET']}));
 
-app.use(express.json());
+router.use(express.json());
 
-app.use(cookieParser("my_passcode"));
-app.use(expressSession({
+router.use(cookieParser("my_passcode"));
+router.use(expressSession({
     secret: "my_passcode",
     cookie: {
         maxAge: 360000
@@ -54,25 +54,52 @@ app.use(expressSession({
 
 
 
-app.use(passport.initialize());
-app.use(passport.session());
+router.use(passport.initialize());
+router.use(passport.session());
 passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
-app.use(connectFlash());
+router.use(connectFlash());
 
-app.use((req, res, next) => {
+router.use((req, res, next) => {
      res.locals.flashMessages = req.flash();
      res.locals.loggedIn = req.isAuthenticated();
      res.locals.currentUser = req.user;
      next();
  })
 
+ router.get("/", homeController.index);
 
 
+router.get("/", homeController.index, homeController.showIndex);
+
+router.get("/friends",homeController.showFriends);
+router.get("/homepage", postsController.index, homeController.showHomepage ,postsController.redirectView), homeController.getFriends;
+router.get("/notifications", postsController.index, homeController.showNotifications ,postsController.redirectView), homeController.getFriends;
+
+//Posts
+router.post("/post",postsController.create, postsController.redirectView, postsController.index);
+router.get("/posts",postsController.index,postsController.indexView)
+router.get("/posts/new", postsController.new);
+router.get("/posts/:id", postsController.show, postsController.showView);
+router.get("/posts/:id/edit", postsController.edit);
+router.put("/post/:id/update", postsController.update, postsController.redirectView);
+router.delete("/posts/:id/delete", postsController.delete, postsController.redirectView);
+
+//Users
+router.get("/users", usersController.index, usersController.indexView);
+router.get("/users/new", usersController.new);
+router.post("/user",  usersController.create, usersController.redirectView);
+
+router.get("/users/login", usersController.login);
+router.post("/users/login", usersController.authenticate);
+router.get("/users/logout", usersController.logout, usersController.redirectView);
 
 
-
+router.get("/users/:id", usersController.show, usersController.showView);
+ router.get("/users/:id/edit", usersController.edit);
+router.put("/users/:id/update" ,usersController.update, usersController.redirectView);
+router.delete("/users/:id/delete", usersController.delete, usersController.redirectView);
 
 
 
@@ -82,13 +109,14 @@ app.use((req, res, next) => {
 
 
 //router.get("/users", usersController.index,usersController.indexView);
-
-
+router.get("/signin",homeController.getSignin);
+router.get("/signup", usersController.getUsersPage);
 // router.post("/user",usersController.saveUser);
 
 
 
-
+router.use(errorController.pageNotFoundError);
+router.use(errorController.interalServerError);
 
 app.use("/", router);
 
